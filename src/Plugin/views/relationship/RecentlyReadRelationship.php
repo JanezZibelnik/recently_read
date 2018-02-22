aa31125b164369eb56b2147c25c4b987e939c114
www-data <www-data@example.com>
2018-02-22 15:49:52 +0000

    Added bundle configuration and filter on relationship configuration

--- src/Plugin/views/relationship/RecentlyReadRelationship.php
+++ src/Plugin/views/relationship/RecentlyReadRelationship.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\recently_read\Plugin\views\relationship;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\relationship\RelationshipPluginBase;
 use Drupal\Core\Session\AccountProxy;
 use Drupal\Core\PageCache\ResponsePolicy\KillSwitch;
+use Drupal\Core\Config\CachedStorage;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
@@ -30,6 +32,13 @@ class RecentlyReadRelationship extends RelationshipPluginBase {
   protected $killSwitch;
 
   /**
+   * Drupal\Core\Config\CachedStorage definition.
+   *
+   * @var \Drupal\Core\Config\CachedStorage
+   */
+  protected $cachedStorage;
+
+  /**
    * RecentlyReadRelationship constructor.
    */
   function __construct(
@@ -37,12 +46,14 @@ class RecentlyReadRelationship extends RelationshipPluginBase {
     $pluginId,
     $pluginDefinition,
     AccountProxy $currentUser,
-    KillSwitch $killSwitch
+    KillSwitch $killSwitch,
+    CachedStorage $cachedStorage
   ) {
     parent::__construct($configuration, $pluginId, $pluginDefinition);
 
     $this->currentUser = $currentUser;
     $this->killSwitch = $killSwitch;
+    $this->cachedStorage = $cachedStorage;
   }
 
   /**
@@ -55,10 +66,51 @@ class RecentlyReadRelationship extends RelationshipPluginBase {
       $pluginDefinition,
     // Load the service required to construct this class.
       $container->get('current_user'),
-      $container->get('page_cache_kill_switch')
+      $container->get('page_cache_kill_switch'),
+      $container->get('config.storage'),
+      $container->get('recently_read')
     );
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function defineOptions() {
+    $options = parent::defineOptions();
+    $options['bundles'] = ['default' => []];
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    $entity_type = $this->definition['recently_read_type'];
+    $typesOptions = false;
+
+    // Read the entity_type configuration and load the types
+    $types = $this->cachedStorage->read('recently_read.recently_read_type.'.$entity_type)['types'];
+
+    // If types are enabled prepare the array for checkboxes options
+    if (isset($types) && !empty($types))
+    {
+      $typesOptions = array_combine($types, $types);
+    }
+
+    if ($typesOptions) {
+      $form['bundles'] = [
+        '#type' => 'checkboxes',
+        '#title' => $this->t('Bundles'),
+        '#default_value' => $this->options['bundles'],
+        '#required' => TRUE,
+        '#options' => $typesOptions,
+      ];
+    }
+
+    $form['#cache']['max-age'] = 0;
+  }
   /**
    * {@inheritdoc}
    */
@@ -78,6 +130,11 @@ class RecentlyReadRelationship extends RelationshipPluginBase {
     }
     else {
       $this->query->addWhere('recently_read', "recently_read_$basetable.user_id", $this->currentUser->id(), "=");
+    }
+
+    // Filter by entity bundles selected while configuring the relationship
+    if(!empty($this->options['bundles'])) {
+      $this->query->addWhere('recently_read', "$basetable.type", array_filter(array_values($this->options['bundles'])), "IN");
     }
   }
 
